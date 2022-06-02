92a2e76fd5c0f9a52048e4fcfa926798f32cbe5f
David Del Sol <Dcastro@gap.com>
2022-06-02 17:43:00 -0600

    fizbuzz

--- sec9/index.js
+++ sec9/index.js
@@ -1,5 +1,21 @@
-let tweet = prompt('write something');
+numbers = []
 
-let lenght = tweet.length;
+for (var i = 0; i <= 100; i++) {
+    if (i % 3  === 0)  {
+        if (i % 5 === 0) {
+            console.log('fizzbuzz')
+            numbers.push('fizzbuzz')
+        } else {
+            console.log('fizz')
+            numbers.push('fizz')
+        }
+    } else if (i % 5 === 0) {
+        console.log('buzz')
+        numbers.push('buzz')
+    } else {
+        console.log(i)
+        numbers.push(i)
+    }
+    
+}
 
-alert('You have written ' + lenght + ' characters, you have ' + (280 - lenght) + ' characters left')
